f747495c993a20559a66433d08071ab42ca95f3a
Alyssa <alyssagieddrobotics@gmail.com>
2016-02-03 19:56:46 -0800

    Modified tank drive with joystick to initiate in open loop mode

--- src/main/java/org/usfirst/frc/team1294/robot/commands/TankDriveWithJoystick.java
+++ src/main/java/org/usfirst/frc/team1294/robot/commands/TankDriveWithJoystick.java
@@ -14,8 +14,9 @@ public class TankDriveWithJoystick extends Command {
     }
 
     // Called just before this Command runs the first time
-    protected void initialize() {}
-
+    protected void initialize() {
+    	Robot.driveBase.setTalonsToOpenLoop();
+    }
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	Robot.driveBase.tankDrive(Robot.oi.getStickLeft(), Robot.oi.getStickRight());
