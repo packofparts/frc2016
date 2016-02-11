27b5825d4b74eeb24b1c24ffbba213911237b278
Austin Jenchi <timtim17@users.noreply.github.com>
2016-02-10 18:49:10 -0800

    Drive correct direction

--- src/main/java/org/usfirst/frc/team1294/robot/commands/AutonomousReachDefense.java
+++ src/main/java/org/usfirst/frc/team1294/robot/commands/AutonomousReachDefense.java
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class AutonomousReachDefense extends CommandGroup {
 	
 	protected static final double DISTANCE_TO_REACH_DEFENSE = 1.0; // TODO determine the correct distance
-	protected static final double SPEED = 0.5;
+	protected static final double SPEED = -0.5;
 	
 	public AutonomousReachDefense() {
 		addSequential(new DriveStraightDistance(SPEED, DISTANCE_TO_REACH_DEFENSE));
