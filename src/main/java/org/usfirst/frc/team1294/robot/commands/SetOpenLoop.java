5fd54a11811d2d02172ed4e1ee3b2ef5107f7ead
John R Lewis <jxlewis@godaddy.com>
2016-02-13 09:36:26 -0800

    Added command to allow switching to open loop

---
+++ src/main/java/org/usfirst/frc/team1294/robot/commands/SetOpenLoop.java
@@ -0,0 +1,35 @@
+package org.usfirst.frc.team1294.robot.commands;
+
+import org.usfirst.frc.team1294.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class SetOpenLoop extends Command {
+
+	public SetOpenLoop() {
+		requires(Robot.driveBase);
+	}
+	
+	@Override
+	protected void initialize() {
+	}
+
+	@Override
+	protected void execute() {
+		Robot.driveBase.setTalonsToOpenLoop();
+	}
+
+	@Override
+	protected boolean isFinished() {
+		return true;
+	}
+
+	@Override
+	protected void end() {
+	}
+
+	@Override
+	protected void interrupted() {
+	}
+
+}
