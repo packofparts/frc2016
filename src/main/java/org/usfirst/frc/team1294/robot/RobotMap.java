4649e67f11dfa59ca55b735b1116792130129919
John R Lewis <jxlewis@godaddy.com>
2016-02-27 13:01:22 -0800

    remapped talons to match the new wiring for 'lucky'

--- src/main/java/org/usfirst/frc/team1294/robot/RobotMap.java
+++ src/main/java/org/usfirst/frc/team1294/robot/RobotMap.java
@@ -9,12 +9,13 @@ import org.usfirst.frc.team1294.robot.utilities.RobotDetector;
  * and either {@code int} or {@code double}.
  */
 public class RobotMap {
-	public static final int leftFrontTalon = 4;
+	public static final int intakeTalon = 1;
+	public static final int spareTalon = 2;
 	public static final int leftBackTalon = 3;
-	public static final int rightFrontTalon = 7;
-	public static final int rightBackTalon = 8;
-	public static final int lowerArmTalon = 5;
-	public static final int upperArmTalon = 6;
+	public static final int leftFrontTalon = 4;
+	public static final int rightFrontTalon = 5;
+	public static final int rightBackTalon = 6;
+	
 	public static final double intakeSpeed = 1;
 	public static final double catapultSpeed = 1;
 	private static final String frontCamera = "cam2";
@@ -23,12 +24,7 @@ public class RobotMap {
 	private static final String backCamera2 = "cam0";
 	private static final double distanceScaler = 5300;
 	private static final double distanceScaler2 = 3533.33;
-	// TODO: Get proper id numbers
-	public static int catapultTalon = 5;
-	public static int intakeTalon = 1;
-	//public static int catapultGearTalon = 7;
-	public static int gearLimitSwitch = 1;
-
+	
 	public static double getDistanceScaler() {
 		if (RobotDetector.getWhichRobot() == RobotDetector.WhichRobot.ROBOT_1) return distanceScaler;
 		else return distanceScaler2;
