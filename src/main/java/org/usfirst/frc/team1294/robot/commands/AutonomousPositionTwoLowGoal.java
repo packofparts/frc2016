9b8c132f5c462f8a289150b29ddc1faad52cac5d
John R Lewis <jxlewis@godaddy.com>
2016-03-09 20:44:24 -0800

    pos 2 low goal

--- src/main/java/org/usfirst/frc/team1294/robot/commands/AutonomousPositionTwoLowGoal.java
+++ src/main/java/org/usfirst/frc/team1294/robot/commands/AutonomousPositionTwoLowGoal.java
@@ -7,18 +7,19 @@ public class AutonomousPositionTwoLowGoal extends CommandGroup {
 
 	public AutonomousPositionTwoLowGoal() {
 		addSequential(new ResetGyro());
+		addSequential(new WaitCommand(0.5));
 		
 		// drive straight forward until at aiming point
-		addSequential(new DriveStraightDistance(-0.7, 5)); // TODO: measure this distance
+		addSequential(new DriveStraightDistance(-0.8, 7.9)); // TODO: measure this distance
 		
 		// turn so that back end is pointing roughly at target
-		addSequential(new TurnToHeading(250)); // TODO: validate this angle
+		addSequential(new TurnToHeading(268)); // TODO: validate this angle
 		
 		// aim at the goal, drive forward, aim at the goal, drive forward
+		//addSequential(new WaitCommand(1));
 		addSequential(new TurnTowardsVisionTarget());
-		addSequential(new DriveStraightDistance(0.7, 1));
-		addSequential(new TurnTowardsVisionTarget());
-		addSequential(new DriveStraightDistance(0.7, 1));
+		addSequential(new DriveStraightDistance(0.8, 2));
+		
 		
 		// shoot the ball
 		addSequential(new TimedEjectBall(1));
