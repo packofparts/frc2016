e6b1f9dc365d74d6a33de5368d5aee39e5394c63
Austin Jenchi <timtim17@users.noreply.github.com>
2016-03-12 20:12:57 -0800

    Remove unneded method

--- src/main/java/org/usfirst/frc/team1294/robot/subsystems/ArmSubsystem.java
+++ src/main/java/org/usfirst/frc/team1294/robot/subsystems/ArmSubsystem.java
@@ -5,7 +5,6 @@ import org.usfirst.frc.team1294.robot.commands.DriveArmWithJoystickCommand;
 
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.command.Subsystem;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * @author Austin Jenchi (timtim17)
@@ -15,26 +14,19 @@ public class ArmSubsystem extends Subsystem {
    * A constant used as the {@code x} value to timeout the command, per the proposal.
    */
   public static final double TIMEOUT = 5;
-  private static double SPEED = 0.5;
+  private static final double SPEED = 0.5;
 
   private CANTalon talon;
 
   public ArmSubsystem() {
     talon = new CANTalon(RobotMap.spareTalon);
-    SmartDashboard.putNumber("arm speed", SPEED);
-  }
-
-  private static void updateSpeed() {
-    SPEED = SmartDashboard.getNumber("arm speed", 0.5);
   }
 
   public void forwards() {
-    updateSpeed();
     talon.set(SPEED);
   }
 
   public void backwards() {
-    updateSpeed();
     talon.set(-SPEED);
   }
 
